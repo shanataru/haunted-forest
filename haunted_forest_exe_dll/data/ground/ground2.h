f966ebc973043bf526d16788b5718b423bd7ce6d
shanataru <shanataru@gmail.com>
2024-08-28 16:59:48 +0100

    Removing exec

--- haunted_forest_exe_dll/data/ground/ground2.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-extern const int planeNAttribsPerVertex;
-extern const int planeNVertices;
-extern const int planeNTriangles;
-extern const float planeVertices[];
-extern const unsigned planeTriangles[];
--- haunted_forest_exe_dll/data/stone/stone.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-extern const int rockNAttribsPerVertex;
-extern const int rockNVertices;
-extern const int rockNTriangles;
-extern const float rockVertices[];
-extern const unsigned rockTriangles[];
